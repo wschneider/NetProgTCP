9eb5860f44fb795e12528db8edffbbed8a4902a3
U-Will-PC\Will <Will@Will-PC.(none)>
2014-03-22 20:26:00 -0400

    Server class added, Hello World implemented

---
+++ Server.java
@@ -0,0 +1,25 @@
+/*
+    Server.java
+    Authors: 
+        Will Schneider
+        Will Kelly
+
+    Purpose:
+        Chat Server receives and handles TCP/UDP connections from clients. 
+        
+    Methods:
+        To Be Described Later
+*/
+
+public class Server
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World\n");
+    }
+    
+    
+    
+    
+    
+}
