9e25b3167deebbb6e8b1edfa3d93f7b030e27b82
wschneider <willschneider.513@gmail.com>
2014-03-24 12:13:59 -0400

    refactoring a bit, I think?

---
+++ netprog/ServerManager.java
@@ -0,0 +1,71 @@
+/*
+    ServerManager.java
+    Authors: 
+        Will Schneider
+        Will Kelly
+
+    Purpose:
+        ServerManager sits as the overhead manager of several Servers (one for 
+        each specified port in the command line arguments). 
+        
+    Methods:
+        To Be Described Later
+*/
+package netprog;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ServerManager
+{
+    //Singleton representation of the server because reasons
+    ServerManager INSTANCE;
+    Thread[] POOL;
+    
+    /*
+        void main:
+            (1) Converts arguments to strings
+            (2) Creates an instance of the server
+            (3) Runs the server
+    */
+    public static void main(String args[])
+    {
+        /*
+            Args are the ports the server is going to listen on... 
+            Those really should be ints, shouldn't they...
+        */
+        int[] ports = new int[args.length];
+        
+        for(int i = 0;i < args.length;i++)
+        {
+            ports[i] = Integer.parseInt(args[i]);
+        }
+        
+        /*
+        TODO: CONTINUE MAIN FUNCTION.
+        */
+        ServerManager a = new ServerManager(ports);
+        a.start();
+        
+    }
+    
+    public ServerManager(int ports[])
+    {
+        
+        POOL = new Thread[ports.length];
+        for(int i=0;i<ports.length;i++)
+        {
+            POOL[i] = new Thread(new Server(ports[i]));
+        }   
+    }
+    
+    public void start()
+    {
+        for(Thread thread : POOL)
+        {
+            thread.start();
+        }
+    }
+    
+}
